132bbbde2003776c6ddaae3ab56dd5f9d5230db4
GabrielRioo <gabrielpaivario@gmail.com>
2026-05-16 16:58:01 -0300

    Adicionando a estrutura inicial do projeto, com o modelo do dominio

--- Backend/RealityHub.Application/obj/Debug/net8.0/RealityHub.Application.AssemblyInfo.cs
+++ Backend/RealityHub.Application/obj/Debug/net8.0/RealityHub.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealityHub.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5ab42483331ae4b8f68727742c5f84753ae1f20")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealityHub.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealityHub.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
